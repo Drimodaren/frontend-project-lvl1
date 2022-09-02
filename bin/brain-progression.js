70b886f53b962bf7275b0fa459d5633e1873ac8b
Drimodaren <men.a.a@yandex.ru>
2022-09-02 10:57:56 +0300

    step_8

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,12 +1,23 @@
 #!/usr/bin/env node
 import readlineSync from "readline-sync";
 import { sayHello } from "../src/utils/sayHello.js";
-import { lastAP, rightAnswer } from "../src/utils/AP.js";
 
 const userName = sayHello();
 console.log("What is the result of the expression?");
 let count = 0;
 while (count !== 3) {
+  const apArr = [];
+  const randomFirstNum = Math.floor(Math.random() * 10);
+  apArr.push(randomFirstNum);
+  const randomApNum = Math.floor(Math.random() * 10);
+  const randomArrlength = Math.floor(Math.random() * (9 - 5 + 1) + 5);
+  for (let i = 0; i <= randomArrlength; i++) {
+    apArr.push(apArr[i] + randomApNum);
+  }
+  const randomArrNum = Math.floor(Math.random() * apArr.length);
+  const rightAnswer = apArr[randomArrNum];
+  apArr[randomArrNum] = "..";
+  const lastAP = apArr.join(" ");
   console.log(`Question: ${lastAP}`);
   const answer = readlineSync.question("Your answer: ");
   if (rightAnswer === eval(answer)) {
