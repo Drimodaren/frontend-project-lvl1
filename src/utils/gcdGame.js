06236ae9011054c41908bc603dc0e58fe3cf6457
Drimodaren <men.a.a@yandex.ru>
2022-11-04 15:35:14 +0400

    fix 0 in gcd

--- src/utils/gcdGame.js
+++ src/utils/gcdGame.js
@@ -2,8 +2,8 @@ import readlineSync from 'readline-sync';
 import NOD from './NOD.js';
 
 const gcdGame = (userName) => {
-  const randomNum1 = Math.floor(Math.random() * 100);
-  const randomNum2 = Math.floor(Math.random() * 100);
+  const randomNum1 = Math.floor(Math.random() * (100 - 1) + 100);
+  const randomNum2 = Math.floor(Math.random() * (100 - 1) + 100);
   const nod = NOD(randomNum1, randomNum2);
   console.log(`Question: ${randomNum1} ${randomNum2}`);
   const answer = readlineSync.question('Your answer: ');
