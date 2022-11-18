9d85f163e41959dfe88bfe228677cfe4d712ca2f
Drimodaren <men.a.a@yandex.ru>
2022-11-18 18:33:27 +0400

    fix_linter_10

--- src/utils/getPrime.js
+++ src/utils/getPrime.js
@@ -1,7 +1,7 @@
 const getPrime = (n) => {
   let resultNum = 0;
   let rightAnswer = '';
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < n; i += 1) {
     if (n % i === 0) resultNum += 1;
   }
   if (resultNum > 1) {
